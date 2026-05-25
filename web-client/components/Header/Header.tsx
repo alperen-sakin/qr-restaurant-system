16a427361cfde071a6dd7128cb2607bade47a006
alperen-sakin <azapkaan@gmail.com>
2026-05-25 05:07:21 +0200

    Use border-borderColor in Header component

--- web-client/components/Header/Header.tsx
+++ web-client/components/Header/Header.tsx
@@ -1,15 +1,14 @@
 import { Utensils } from "lucide-react";
-import React from "react";
 
 const Header = () => {
   return (
-    <header className="bg-surface w-full flex flex-row items-center justify-between p-4 border-b border-b-primary">
+    <header className="bg-surface w-full flex flex-row items-center justify-between p-4 border-b border-borderColor">
       <div className="flex flex-row items-center gap-4">
         <Utensils size={24} color="var(--color-primary)" />
         <h1 className="text-primary text-xl font-semibold ">Sync Plate</h1>
       </div>
 
-      <div className="bg-table text-sm font-bold text-primary p-2 rounded-full border border-primary">
+      <div className="bg-table text-sm font-bold text-primary p-2 rounded-full border border-borderColor">
         <h2>TABLE 12</h2>
       </div>
     </header>
