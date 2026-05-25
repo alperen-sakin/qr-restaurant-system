a5012880c61c94d4453ea3f9ad93ddac92eb49b4
alperen-sakin <azapkaan@gmail.com>
2026-05-25 05:06:22 +0200

    Redirect root page to /menu

--- web-client/app/page.tsx
+++ web-client/app/page.tsx
@@ -1,26 +1,5 @@
-"use client";
+import { redirect } from "next/navigation";
 
-import { getProducts } from "@/service/productService";
-import { useState, useEffect } from "react";
-import Menu from "@/components/Menu/Menu";
-
-const page = () => {
-  const [products, setProducts] = useState<any[]>([]);
-
-  useEffect(() => {
-    const fetchProducts = async () => {
-      const data = await getProducts();
-      setProducts(data);
-    };
-
-    fetchProducts();
-  }, []);
-
-  return (
-    <div className="flex-1">
-      <Menu />
-    </div>
-  );
-};
-
-export default page;
+export default function Home() {
+  redirect("/menu");
+}
