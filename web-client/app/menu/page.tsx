41b66a046eddb996650a97f1b815a7609c56ad67
alperen-sakin <azapkaan@gmail.com>
2026-05-25 05:06:00 +0200

    Add cart, menu, and search pages

---
+++ web-client/app/menu/page.tsx
@@ -0,0 +1,12 @@
+import Menu from "@/components/Menu/Menu";
+import React from "react";
+
+const MenuPage = () => {
+  return (
+    <main className="flex-1">
+      <Menu />
+    </main>
+  );
+};
+
+export default MenuPage;
---
+++ web-client/app/search/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const page = () => {
+  return <div className="flex-1">Search</div>;
+};
+
+export default page;
