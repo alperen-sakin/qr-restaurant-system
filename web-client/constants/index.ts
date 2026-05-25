b2682cc835e9a76d799b683671f52099b7225fcd
alperen-sakin <azapkaan@gmail.com>
2026-05-25 03:36:51 +0200

    Add NAV_ITEMS constant for navigation

---
+++ web-client/constants/index.ts
@@ -0,0 +1,5 @@
+export const NAV_ITEMS = [
+  { id: "menu", title: "Menu", href: "/menu" },
+  { id: "search", title: "Search", href: "/search" },
+  { id: "cart", title: "Cart", href: "/cart" },
+];
