de35a3d1329c9712433537fc59f7aa1ce9163098
alperen-sakin <azapkaan@gmail.com>
2026-06-18 10:03:47 +0200

    Remove unused setValue import in HistoryScreen

--- kitchen-app/app/src/main/java/com/example/kitchenapp/presentation/historyScreen/HistoryScreen.kt
+++ kitchen-app/app/src/main/java/com/example/kitchenapp/presentation/historyScreen/HistoryScreen.kt
@@ -7,7 +7,6 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
